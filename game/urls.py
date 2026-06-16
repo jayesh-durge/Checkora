8c0bc06cefcd3708e0b15d108764e09a95cb7246
somanshu5011 <somanshumahajan@gmail.com>
2026-06-16 10:11:11 +0530

    added forum endpoints in urls

--- game/urls.py
+++ game/urls.py
@@ -52,4 +52,10 @@ urlpatterns = [
     path("achievement/<int:achievement_id>/download/", views.download_badge, name="download_badge",),
     path("feature-badge/<int:achievement_id>/", views.feature_badge, name="feature_badge"),
     path("remove-featured-badge/<int:badge_id>/", views.remove_featured_badge, name="remove_featured_badge"),
+
+    # Community Forum
+    path("forum/", views.forum_list, name="forum"),
+    path("forum/new/", views.forum_new, name="forum_new"),
+    path("forum/<int:discussion_id>/", views.forum_detail, name="forum_detail"),
+    path("forum/<int:discussion_id>/reply/", views.forum_reply, name="forum_reply"),
 ]
